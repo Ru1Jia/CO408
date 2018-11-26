b1b235d60d731f57fbdb14086c47ade62092fe8b
Ru1Jia <45341419+Ru1Jia@users.noreply.github.com>
2018-11-26 05:08:30 +0000

    Update ot.py

--- ot.py
+++ ot.py
@@ -9,10 +9,13 @@ OBLIVIOUS_TRANSFERS = True
 if OBLIVIOUS_TRANSFERS: # __________________________________________________
     primeGroup = util.PrimeGroup()
 
-    # alice generates c from G, bob generates x from (Z/qZ)
+    # alice generates c from G
+    # bob generates x from (Z/qZ).
     def generate_random_int():  return primeGroup.rand_int()
 
     # bob sends h_b to alice
+    # INPUT: x, c, b
+    # OUTPUT: h_0
     def generate_h_b(x, c_from_G, b_chosen_by_bob):
         # x is generated from (Z/qZ) in main
         # bob generates a pair of public keys: h_b and h_1-b
@@ -20,24 +23,25 @@ if OBLIVIOUS_TRANSFERS: # __________________________________________________
         h_b_1 = primeGroup.mul(c_from_G, primeGroup.inv(h_b_0))
         # send the h_b
         h_b = []
-        if b_chosen_by_bob == 0:    h_b.append(h_b_0)
-        if b_chosen_by_bob == 1:    h_b.append(h_b_1)
+        if b_chosen_by_bob == 0:    return h_b_0
+        if b_chosen_by_bob == 1:    return h_b_1
         else:
             print("ERROR: bob generates public keys !")
 
-        return h_b
 
     # alice sends c, e0, e1 to bob
-    def send_parameters(c, h0, m_0, m_1):
+    # INPUT: c, h_0, m_0, m_1
+    # OUTPUT: c_1, e_0, e_1
+    def send_parameters(c, h_0, m_0, m_1):
         # generate h1 by c/h0
-        h1 = primeGroup.mul(c, primeGroup.inv(h0))
+        h_1 = primeGroup.mul(c, primeGroup.inv(h_0))
         # alice generates k from (Z/qZ)
         k = primeGroup.rand_int()
         # alice generate a public key by g^k
         c_1 = primeGroup.gen_pow(k)
         # Calculate e_0 and e_1
-        e_0 = util.xor_bytes(m_0, util.ot_hash(primeGroup.pow(h0, k), len(m_0)))
-        e_1 = util.xor_bytes(m_1, util.ot_hash(primeGroup.pow(h1, k), len(m_1)))
+        e_0 = util.xor_bytes(m_0, util.ot_hash(primeGroup.pow(h_0, k), len(m_0)))
+        e_1 = util.xor_bytes(m_1, util.ot_hash(primeGroup.pow(h_1, k), len(m_1)))
         #
         parameters_AliceToBob = []
         parameters_AliceToBob.append(c_1)
@@ -46,7 +50,8 @@ if OBLIVIOUS_TRANSFERS: # __________________________________________________
 
         return parameters_AliceToBob
 
-
+    # INPUT: x, c_1, e_0, e_1, b_chosen_by_bob
+    # OUTPUT: m_b
     def calculate_m_b(x, c_1, e_0, e_1, b_chosen_by_bob):
         # the same x with generate_h_b
         # calculate m_b and m_1-b
